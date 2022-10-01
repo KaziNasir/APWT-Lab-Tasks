96615fc273bb4081170b8cc107cae573b86193d4
KaziNasir <shoumikkazi@gmail.com>
2022-10-02 00:12:57 +0600

    complete features v1.0

--- Mid/APWT_TASK_1/routes/web.php
+++ Mid/APWT_TASK_1/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ServicesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/services',[ServicesController::class, 'services'])->name('services');
+Route::get('/aboutUs',[PagesController::class, 'aboutUs'])->name('aboutUs');
+Route::get('/contactUs',[PagesController::class, 'contactUs'])->name('contactUs');
+Route::get('/ourTeams',[PagesController::class, 'ourTeams'])->name('ourTeams');
